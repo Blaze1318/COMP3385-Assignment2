babee146b96a7c7a3f180ae8388e635039a974a8
BlazeTheCoder <39748904+Blaze1318@users.noreply.github.com>
2020-11-13 13:48:29 -0400

    Add files via upload

--- 400001784-framework/app/models/CoursesModel.php
+++ 400001784-framework/app/models/CoursesModel.php
@@ -60,13 +60,13 @@ class CoursesModel extends Model
 
 	public function findCourses():array
 	{
-		$sql = "SELECT courses.course_name, faculty_department.faculty_dept_name, courses.course_image, instructors.instructor_name 
-				FROM faculty_department, courses 
-                INNER JOIN faculty_dept_courses ON courses.course_id = faculty_dept_courses.course_id
+		$sql = "SELECT DISTINCT courses.course_name, faculty_department.faculty_dept_name, courses.course_image, instructors.instructor_name 
+				FROM faculty_department
+                INNER JOIN faculty_dept_courses ON faculty_department.faculty_dept_id= faculty_dept_courses.faculty_dept_id
+                INNER JOIN courses ON faculty_dept_courses.course_id = courses.course_id
 				INNER JOIN course_instructor ON courses.course_id = course_instructor.course_id 
 				INNER JOIN instructors ON course_instructor.instructor_id = instructors.instructor_id
-				ORDER BY courses.course_access_count DESC
-				LIMIT 21";
+				";
 		$stmt = $this->databaseConnection->prepare($sql);
         $stmt->execute();
         $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
--- 400001784-framework/app/models/UsersModel.php
+++ 400001784-framework/app/models/UsersModel.php
@@ -37,11 +37,16 @@
 
         public function getCourses():array
         {
-             $sql = "SELECT courses.course_name, courses.course_description, courses.course_image, instructors.instructor_name 
-            FROM courses 
+            SessionClass::create();
+            $user = $_SESSION['LoggedIn'];
+            $sql = "SELECT DISTINCT courses.course_name, faculty_department.faculty_dept_name, courses.course_image, instructors.instructor_name 
+            FROM faculty_department 
+            INNER JOIN faculty_dept_courses ON faculty_department.faculty_dept_id= faculty_dept_courses.faculty_dept_id 
+            INNER JOIN courses ON faculty_dept_courses.course_id = courses.course_id 
             INNER JOIN course_instructor ON courses.course_id = course_instructor.course_id 
             INNER JOIN instructors ON course_instructor.instructor_id = instructors.instructor_id 
-            WHERE courses.course_id = users_courses.course_id";
+            INNER JOIN user_courses ON courses.course_id = user_courses.course_id 
+            INNER JOIN users ON user_courses.email = users.email WHERE user_courses.email = '".$user."'";
             $stmt = $this->databaseConnection->prepare($sql);
             $stmt->execute();
             $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
